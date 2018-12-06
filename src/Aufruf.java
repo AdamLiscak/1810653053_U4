56474acbd634a7fff9446a218bcfea6c3e60a623
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-12-06 11:49:12 +0100

    Aufgabe4

--- src/Aufruf.java
+++ src/Aufruf.java
@@ -3,6 +3,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 public class Aufruf {
     public static void main(String[] args) {
@@ -14,7 +15,15 @@ public class Aufruf {
         scroll.setTitle("My Book");
         System.out.print("Das Buch hat "+scroll.getPages()+" Seiten, wurde am Tue ");
         System.out.print(scroll.getReleased()+ " veröffentlicht und hat den Titel "+scroll.getTitle());
-        List myList<Book> = new ArrayList<>();
-        
+        Book b1= new Book()
+                b1.setPages(200);
+        b1.setIsbn(String.valueOf(Random.nextInt(9000) + 100));
+
+        Book b2=new Book()
+        Book b3=new Book()
+        Book b4=new Book()
+        Book b5=new Book()
+        List myList<Book> = new ArrayList<>(b1,b2,b3,b4,b5);
+
     }
 }
